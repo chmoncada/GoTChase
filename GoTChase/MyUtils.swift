8f321aa31699459caaf16f73481a461592a31af9
Charles Moncada <chmoncada@me.com>
2018-03-11 22:09:00 -0300

    Add MyUtils file

---
+++ GoTChase/MyUtils.swift
@@ -0,0 +1,73 @@
+//
+//  MyUtils.swift
+//  GoTChase
+//
+//  Created by Charles Moncada on 11/03/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (left: inout CGPoint, right: CGPoint) {
+    left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+    left = left - right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (left: inout CGPoint, right: CGPoint) {
+    left = left * right
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (point: inout CGPoint, scalar: CGFloat) {
+    point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= ( left: inout CGPoint, right: CGPoint) {
+    left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+func /= (point: inout CGPoint, scalar: CGFloat) {
+    point = point / scalar
+}
+
+extension CGPoint {
+    
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+    
+    var angle: CGFloat {
+        return atan2(y, x)
+    }
+}
